84e4f7e5cef48f4039aeb3e7b9811daeb5a684b6
Ohlala <924926395@qq.com>
2017-10-31 11:51:46 +0800

    can't understand recoverdata

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -20,17 +20,32 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-for i = 1 : size(X,1)
-    idx(i) = 1;
-    distance = sum((X(i,:)-centroids(1,:)).^2);
-    for j = 1 : length(centroids)
-        dis_temp = sum((X(i,:)-centroids(j,:)).^2);
-        if dis_temp < distance
-            distance = dis_temp;
-            idx(i) = j;
-        end
-    end
+%????????????????????????????????????????????
+% for i = 1 : size(X,1)
+%     idx(i) = 1;
+%     distance = sum((X(i,:)-centroids(1,:)).^2);
+%     for j = 1 : length(centroids)
+%         dis_temp = sum((X(i,:)-centroids(j,:)).^2);
+%         if dis_temp < distance
+%             distance = dis_temp;
+%             idx(i) = j;
+%         end
+%     end
+% end
+
+N = size(X,2);	
+
+for i=1:length(X)
+	for k=1:K		
+		for n=1:N	
+			a(n)=(X(i,n)-centroids(k,n)).^2;	% Calc distance from X to centroids (sum to N, not just 2)
+		end
+		dist(k)=sum(a);		% Distance from point X to centroids 
+	end
+	[r,c] = min(dist);		% Find the index of the min distance btw X and centroids
+	idx(i) = c;				% Assign for each X which centroid it belongs to
 end
+
 % =============================================================
 
 end
